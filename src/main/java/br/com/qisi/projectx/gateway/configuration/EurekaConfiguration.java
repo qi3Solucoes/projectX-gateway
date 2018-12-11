066cf7124960c0ceb676940603c9e713ae60930d
Rodrigo Sene <rodrigo.sene@qisi.com.br>
2018-12-11 11:20:13 -0200

    improve eureka setup

--- src/main/java/br/com/qisi/projectx/gateway/configuration/EurekaConfiguration.java
+++ src/main/java/br/com/qisi/projectx/gateway/configuration/EurekaConfiguration.java
@@ -1,6 +1,8 @@
 package br.com.qisi.projectx.gateway.configuration;
 
 import com.netflix.appinfo.AmazonInfo;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,9 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class EurekaConfiguration {
 
+  @Value("${server.port}")
+  int port;
+
   @Bean
   @Profile("!default")
   public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
@@ -18,7 +23,7 @@ public class EurekaConfiguration {
     info.getMetadata().put(AmazonInfo.MetaDataKey.publicHostname.getName(), info.get(AmazonInfo.MetaDataKey.publicIpv4));
     config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
     config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
-    config.setNonSecurePort(8080);
+    config.setNonSecurePort(port);
     config.setDataCenterInfo(info);
     return config;
   }
